773e857eef9a178106df84e293baaf3ad12a10f9
Alikley <alikley933@gmail.com>
2023-10-30 16:15:20 +0330

    New

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,6 +1,8 @@
 import { useForm } from 'react-hook-form'
 import { LoginContext } from '../../../context/LoginContext';
 import { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import './login.scss'
 interface UserLog {
   login: () => void;
 }
@@ -38,7 +40,7 @@ function Login() {
   }
   return (
     <div className='login'>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      {/* <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="email">Email</label>
         <input type="text"  id='firstName' {...register("email" ,{
           required:{
@@ -64,7 +66,56 @@ function Login() {
         <button type='button' onClick={() => reset()}>Rest</button>
 
 
-      </form>
+      </form> */}
+
+
+       <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> 
+
+        <div className="signin"> 
+
+          <div className="content"> 
+
+          <h2>Sign In</h2> 
+
+          <form onSubmit={handleSubmit(onSubmit)} className='form'>
+            <div className="inputBox">
+              <label htmlFor="email">Email</label>
+                <input type="text"  id='firstName' {...register("email" ,{
+                  required:{
+                    value:true,
+                    message:"Please Enter your email"
+                  }
+                })} />
+                <p style={{color:"white",background:"#0f0",display:"block"}}>{errors.email?.message}</p>
+            </div>
+            
+
+            <div className="inputBox">
+              <label htmlFor="password">Password</label>
+                <input type="password" id='lastName' {...register("password",{
+                  required:{
+                    value:true,
+                    message:"Please Enter your email"
+                  }
+                })}/>
+                <p style={{color:"white",background:"#0f0",display:"block"}}>{errors.password?.message}</p>
+            </div>
+             
+            <div className="links"> <NavLink to="#">Forgot Password</NavLink> <NavLink to="#">Signup</NavLink></div>
+
+            <div className="inputBox">
+              <button  type='submit' disabled={!isDirty || !isValid} >Click me</button>
+            </div>
+              {/* <button type='button' onClick={() => reset()}>Rest</button> */}
+
+
+            </form> 
+
+          </div> 
+
+        </div> 
+
+      </section> 
     </div>
   )
 }
